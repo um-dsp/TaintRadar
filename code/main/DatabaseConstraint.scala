d1dfb2603c8af8ad8515f1495541248a78be5bb9
UMD-User <elirizk@umich.edu>
2023-10-25 22:48:57 -0400

    db-constraint 6
    Former-commit-id: fbdf27be7a1a9afb123a746e119b12b8dada029d

--- code/main/DatabaseConstraint.scala
+++ code/main/DatabaseConstraint.scala
@@ -4,6 +4,7 @@ import com.github.tototoshi.csv._
 class DatabaseConstraint(val cpg: Cpg) {
 
     val removeChars = Set(',', '"', '\\', '`')
+    val calculationPattern = "\\s*([-+=/*><!])\\s*".r
 
     var getQueryMap = collection.mutable.Map[AstNode, DBQuery]()
     def getQuery(node: AstNode, output: DBQuery = DBQuery(), depth: Int = 0): DBQuery = {
@@ -46,6 +47,7 @@ class DatabaseConstraint(val cpg: Cpg) {
     }
 
     def labelQuery(query: DBQuery, schema: scala.collection.mutable.Map[String, Map[String, Boolean]]): QueryLabel.Value = {
+        println(query)
         val tables: List[String] = schema.keys.toList
         query.getQueryType() match {
             case QueryType.OtherQuery => QueryLabel.SafeQuery
@@ -61,21 +63,15 @@ class DatabaseConstraint(val cpg: Cpg) {
                 else {
                 val queryColumns = schema(queryTableName) 
                 val unsafeColumns = schema(queryTableName).filterNot(_._2).keys.toList
-                // for (scope <- queryScope) {
-                //     if (scope == "*") val unsafeScope = unsafeColumns
-                //     else if (!safeSQLFunctions.exists(scope.contains(_)) && unsafeColumns.exists(scope.contains(_))) {
-                //         val unsafeScope = unsafeColumns.map(scope.contains(_)).zipWithIndex.filter(_._1==true).map(_._2).collect(unsafeColumns(_))
-                //     }
-                // }
                 if (queryScope.map(scope => (scope == "*" && !unsafeColumns.isEmpty) || (!safeSQLFunctions.exists(scope.contains(_)) && unsafeColumns.exists(scope.contains(_)))).contains(true))
                     QueryLabel.UnsafeQuery
                 else QueryLabel.SafeQuery
                 }
             }
             case QueryType.InsertQuery => {
-                val queryCode = query.getQueryCode.mkString(" ").replace("values(", "values ( ").split(" ") 
-                val queryTable = queryCode.dropWhile(_.toLowerCase() != "into").drop(1).takeWhile(_.toLowerCase() != "values")
-                val queryValues = queryCode.dropWhile(_.toLowerCase() != "values").drop(1).takeWhile(_.toLowerCase() != ";")
+                val queryCode = query.getQueryCode.mkString(" ").split(" ") 
+                val queryTable = queryCode.dropWhile(_.toLowerCase() != "into").drop(1).takeWhile(!_.toLowerCase().contains("values"))
+                val queryValues = queryCode.dropWhile(!_.toLowerCase().contains("values")).drop(1).takeWhile(token => !token.toLowerCase().contains(";") && token.toLowerCase!="where")
                 val queryTableNames = tables.map(x => queryTable.map(_.filter(!removeChars.contains(_))).exists(x.contains)).zipWithIndex.filter(_._1==true).map(_._2).collect(tables(_)).l
                 if (queryTableNames.isEmpty) {
                     // Unable to find a matching table from the query
@@ -83,15 +79,20 @@ class DatabaseConstraint(val cpg: Cpg) {
                     QueryLabel.UnsafeQuery
                 }
                 else {
-                    val tableColumns = queryTableNames.map(schema.get(_).get.keys)
-                    // get most likely table
-                    val mostLikelyIndex = tableColumns.map(_.toList.map(s => queryCode.exists({
-                        val regex = s"(?<!\\p{Alnum})$s(?![\\p{Alnum}])".r
-                        regex.findFirstIn(_).isDefined}))).map(_.filter(_==true).size).zipWithIndex.maxBy(_._1)._2 
-                    val queryTableName = queryTableNames(mostLikelyIndex)
+                    val queryTableName = {
+                        if (queryTableNames.size == 1) queryTableNames(0)
+                        else {
+                            val tableColumns = queryTableNames.map(schema.get(_).get.keys)
+                            // get most likely table
+                            val mostLikelyIndex = tableColumns.map(_.toList.map(s => queryCode.exists({
+                                val regex = s"(?<!\\p{Alnum})$s(?![\\p{Alnum}])".r
+                                regex.findFirstIn(_).isDefined}))).map(_.filter(_==true).size).zipWithIndex.maxBy(_._1)._2 
+                            queryTableNames(mostLikelyIndex)
+                        }
+                    }
                     val sortedColumns: List[String] = {
                         if (queryTable.mkString(" ").contains("("))
-                            queryTable.dropWhile(!_.contains("(")).drop(0).toList
+                            queryTable.dropWhile(!_.contains("(")).drop(0).toList.map(_.replace("(", "").replace(")", ""))
                         else list_schema.filter(_(0) == queryTableName).map(_(1))
                         } 
                     val unsafeColumns = schema(queryTableName).filterNot(_._2).keys.toList
@@ -101,11 +102,18 @@ class DatabaseConstraint(val cpg: Cpg) {
                     if (unsafeColumns.isEmpty || !insertValuesUnsafe.contains(true))
                         QueryLabel.SafeQuery
                     else {
-                        val valuesParsed = queryValues.map(token => if ("[^a-zA-Z0-9 ]".r.replaceAllIn(token, "").size==0) "" else token).filterNot(_.isEmpty) 
-                        if (valuesParsed.size != sortedColumns.size) QueryLabel.UnsafeQuery
+                        val valuesParsed =  calculationPattern.replaceAllIn(queryValues.mkString(" "), matchResult => "CALC_SPACE" + matchResult.group(1) + "CALC_SPACE").split(" ").filter(_.exists(_.isLetterOrDigit)).toList
+                        if (valuesParsed.size < sortedColumns.size) QueryLabel.UnsafeQuery
                         else {
                             val unsafeIndices = unsafeColumns.map(sortedColumns.indexOf(_))
-                            val setUnsafeNodes = query.data.flatMap(x => unsafeIndices.map(valuesParsed(_)).map(searchNode(x, _))).filterNot(_==None)
+                            // val setUnsafeNodes = unsafeIndices.map(valuesParsed(_)).map(query.searchNodeFromQuery(_)).filterNot(_==None)
+                            // val rawToParsedMapping = valuesParsed.zipWithIndex.map(
+                            //     parsedToken => queryValues.zipWithIndex.map(
+                            //             rawToken => if (parsedToken._1.contains(rawToken._1)) parsedToken._2 else -1)
+                            //     ).reduce((array1, array2) => 
+                            //             array1.zip(array2).map { case (x, y) => if (x > y) x else y }) 
+                            // val unsafeRawTokens = queryValues.zipWithIndex.filter(rawToken => unsafeIndices.contains(rawToParsedMapping(rawToken._2))).map(_._1)
+                            val setUnsafeNodes = unsafeIndices.map(valuesParsed.lift(_).getOrElse("").replace("CALC_SPACE", " ")).map(query.searchNodeFromQuery(_)).filterNot(_==None)                         
                             if (setUnsafeNodes.map(s.isSanitized(_)).contains(false))
                                 QueryLabel.UnsafeQuery
                             else QueryLabel.SafeQuery
