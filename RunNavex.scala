2ae3dcecfe34b8e5adb50dd6e1a6f00c86cc77f6
Firas Ben Hmida <104000432+Koubraa@users.noreply.github.com>
2025-08-24 13:31:39 -0400

    Delete RunNavex.scala

--- RunNavex.scala
+++
@@ -1,8 +0,0 @@
-@main def exec(path: String) = {
-    importCpg(path)
-    // importCpg("/home/umd-user/joern/workspace/jenkinscpg.bin/cpg.bin.tmp")
-    val n = new NavexMain(cpg)
-    // save
-    // n.getStats() 
-    n.outputPaths(false)   
-}
